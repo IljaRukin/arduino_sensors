44b355be5a706a1bb45985f18335ba5d4c474a0b
IljaRukin <74410869+IljaRukin@users.noreply.github.com>
2023-05-31 00:07:21 +0200

    implement cycling ir tv remote comands for new library version 4.1.2

--- tv_remote/tv_remote.ino
+++ tv_remote/tv_remote.ino
@@ -1,32 +1,71 @@
-#include <Arduino.h>
-#include <IRremote.hpp>
+//arduino TV remote
 
 //#define IR_RECEIVE_PIN 2 //PD2 (for receive dump)
 #define IR_SEND_PIN 3 //PD3
 
-/*
- * commands
-on-off - 3D
-vol up - 20
-vol down - 21
-silent - 32
-AV - 5
-TV - 30
-1,2,3,4,5,6,7,8,9,0 - 10-19
-prog up - 34
-prog down - 35
-ok - 49
-*/
+#include <Arduino.h>
+#include <IRremote.hpp>
+
+#define DISABLE_CODE_FOR_RECEIVER // Disables restarting receiver after each send. Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not used.
+//#define SEND_PWM_BY_TIMER         // Disable carrier PWM generation in software and use (restricted) hardware PWM.
+//#define USE_NO_SEND_PWM           // Use no carrier PWM, just simulate an active low receiver signal. Overrides SEND_PWM_BY_TIMER definition
+
+#include <IRremote.hpp> // include the library
 
 void setup() {
-    IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    Serial.print(F("Send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+
+    /*
+     * The IR library setup. That's all!
+     */
+//    IrSender.begin();
 }
 
 uint16_t sAddress = 0x8;
-uint8_t sCommand = 0x32;
-uint8_t sRepeats = 0;
+int sCommand = 0;
+uint8_t sRepeats = 3;
 
 void loop() {
+    //if(Serial.available() != 0) {
+    //sCommand = Serial.readStringUntil('\n').toInt();
+    for(int sCommand=0; sCommand<80; sCommand++){
+    Serial.println();
+    Serial.print(F("send: address=0x"));
+    Serial.print(sAddress, HEX);
+    Serial.print(F(", command=0x"));
+    Serial.print(sCommand, HEX);
+    Serial.print(F(", repeats="));
+    Serial.print(sRepeats);
+    Serial.println();
+    Serial.flush();
+
+    Serial.println("sendNEC");
+    Serial.flush();
+    IrSender.sendNEC(sAddress & 0xFFF, sCommand, sRepeats);
+    delay(500);
+    Serial.println("sendJVC");
+    Serial.flush();
+    IrSender.sendJVC(sAddress & 0xFFF, sCommand, sRepeats);
+    delay(500);
+//    Serial.println("sendSAMSUNG");
+//    Serial.flush();
+//    IrSender.sendSAMSUNG(sAddress & 0xFFF, sCommand, sRepeats);
+//    delay(500);
+    Serial.println("sendSony");
+    Serial.flush();
+    IrSender.sendSony(sAddress & 0xFFF, sCommand, sRepeats);
+    delay(500);
+    Serial.println("sendPanasonic");
+    Serial.flush();
     IrSender.sendPanasonic(sAddress & 0xFFF, sCommand, sRepeats);
     delay(500);
+    }
+    //delay(500);
 }
